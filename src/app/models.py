4208972bdde0ed73c16b90a8ba118a5d644ea6a1
Alexander Petrov <petrov.aa@phystech.edu>
2019-08-16 23:47:01 +0300

    add voice model

--- src/app/models.py
+++ src/app/models.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
@@ -13,3 +14,20 @@ class Chat(Base):
     telegram_chat_id = Column(String(32), index=True, unique=True)
     # Состояние чата пользователя с ботом
     state = Column(String(255))
+
+
+class Voice(Base):
+    """
+    Войс.
+    """
+    __tablename__ = 'voice'
+    id = Column(Integer, primary_key=True)
+    file_id = Column(String(255))
+    # Идентификатор телеграм автора войса
+    author_id = Column(String(32))
+    # Имя и фамилия отправителя войса. Кешируем, чтобы не ходить за каждым автором в телеграм
+    author_first_name = Column(String(255))
+    author_last_name = Column(String(255))
+    # Идентификатор отправителя в бота
+    sender_id = Column(Integer, ForeignKey("chat.id", ondelete="CASCADE"))
+    sender = relationship("Chat", cascade="all, delete-orphan")
