17b76541c7b8bb94cd3a9f631ecb7b5daf770b0d
Alexander Petrov <petrov.aa@phystech.edu>
2019-08-16 22:45:43 +0300

    add chat table

--- src/app/models.py
+++ src/app/models.py
@@ -1,3 +1,10 @@
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+
+
+class Chat(Base):
+    __tablename__ = 'chat'
+    id = Column(Integer, primary_key=True)
+    telegram_chat_id = Column(String(32), index=True, unique=True)
