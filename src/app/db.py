cead8e5ba2a654a0bb08d5b4282e40a41a8b2160
Alexander Petrov <petrov.aa@phystech.edu>
2019-08-16 22:49:08 +0300

    low-level db exchange module

---
+++ src/app/db.py
@@ -0,0 +1,72 @@
+"""
+Низкоуровневые взаимодействия с базой данных
+"""
+
+from contextlib import contextmanager
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+
+from app import config
+
+
+__engine = create_engine(config.DB_URL, pool_pre_ping=True)
+
+__SessionFactory = sessionmaker(bind=__engine)
+
+__Session = scoped_session(__SessionFactory)
+
+
+@contextmanager
+def get_flush_session():
+    session = __Session()
+    try:
+        yield session
+        session.flush()
+    except Exception as e:
+        session.rollback()
+        raise
+
+
+def flush_session(func):
+    """
+    Декторатор для функций, в которых необходимо производить манипуляции с БД.
+    Вызывает функци с дополнительным аргументом `session`, который можно использовать
+    для запросов к БД. После окончания выполнения функции всегда вызвает `flush` сессии.
+
+    :param func: Декорируемая функция
+    :return:
+    """
+    def decorated(*args, **kwargs):
+        with get_flush_session() as session:
+            return func(*args, session=session, **kwargs)
+    return decorated
+
+
+@contextmanager
+def get_commit_session():
+    session = __Session()
+    try:
+        yield session
+        session.commit()
+    except Exception as e:
+        session.rollback()
+    finally:
+        session.close()
+        __Session.remove()
+
+
+def commit_session(func):
+    """
+    Декторатор для функций, в которых необходимо производить манипуляции с БД.
+    Вызывает функци с дополнительным аргументом `session`, который можно использовать
+    для запросов к БД. После окончания выполнения функции всегда вызвает `commit` сессии.
+    Данный декторатор должен применяться у функций самого верхнего уровня, поскольку
+    после окончания работы декорируемой функции сессия завершается.
+
+    :param func:
+    :return:
+    """
+    def decorated(*args, **kwargs):
+        with get_commit_session() as session:
+            return func(*args, **kwargs, session=session)
+    return decorated
