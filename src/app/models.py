f56e6507ed5056a660fcf2a8a5a9f07b9a527c30
Alexander Petrov <petrov.aa@phystech.edu>
2019-08-16 23:26:30 +0300

    add chat state

--- src/app/models.py
+++ src/app/models.py
@@ -11,3 +11,5 @@ class Chat(Base):
     __tablename__ = 'chat'
     id = Column(Integer, primary_key=True)
     telegram_chat_id = Column(String(32), index=True, unique=True)
+    # Состояние чата пользователя с ботом
+    state = Column(String(255))
